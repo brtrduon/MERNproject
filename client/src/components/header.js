a0f3e03b81c45e105b3dface89808e7f2eb359fe
brtrduon <brianduongt@gmail.com>
2018-03-21 14:56:30 -0700

    added bio

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,16 +4,19 @@ import { Link } from 'react-router';
 class Header extends Component {
     renderLinks() {
         return [
-            <li className='nav-item' key={4}>
-                <Link className='navbar-brand' to='/'>Welcome</Link>
-            </li>,
             <li className='nav-item' key={1}>
-                <Link className='nav-link' to='/about'>About</Link>
+                <Link className='navbar-brand' to='/'>Jamie's Kitchen</Link>
             </li>,
             <li className='nav-item' key={2}>
-                <Link className='nav-link' to='/location'>Location</Link>
+                <Link className='nav-link' to='/about'>About</Link>
             </li>,
             <li className='nav-item' key={3}>
+                <Link className='nav-link' to='/location'>Location</Link>
+            </li>,
+            <li className='nav-item' key={4}>
+                <Link className='nav-link' to='/menu'>Menu</Link>
+            </li>,
+            <li className='nav-item' key={5}>
                 <Link className='nav-link' to='/reserve'>Reservations</Link>
             </li>
         ];
@@ -21,7 +24,7 @@ class Header extends Component {
 
     render() {
         return (
-            <nav className='navbar navbar-light'>
+            <nav className='navbar-light'>
                 <ul className='navbar navbar-nav'>
                     {this.renderLinks()}
                 </ul>
--- client/src/components/views/about.js
+++ client/src/components/views/about.js
@@ -4,7 +4,13 @@ class About extends Component {
     render() {
         return (
             <div>
-                insert stuff about store history and store owner here
+                <strong>Jamie Lynn Sato</strong>
+                <br></br>
+                <br></br>
+                Born and raised in Tokyo, Japan, Jamie Lynn Sato discovered her talent and passion for cooking during her pre-teen years. After completing her bachelor's degree at the University of California, Berkeley, Jamie worked as Head Chef at several acclaimed restaurants in the Bay Area including, but not limited to, Fleur de Lys in San Francisco, Firehouse No.1 Gastropub in San Jose, and Woood Tavern in Oakland. 
+                <br></br>
+                <br></br>
+                Wanting to bring together a mixture of Japanese and American cuisine and to expand upon her passion for cooking, Jamie opened Jamie's Kitchen, a 100-seat neighborhood restaurant, in the heart of Mountain View, CA. "I'm really excited to bring together such a hybrid of taste and culture to the community," Jamie explains. "It's always been something I wanted to do to since I was a little girl to create delectable dishes that no one has seen before that everyone can enjoy, regardless if they are a member of our community or if they are just visiting." 
             </div>
         );
     }
